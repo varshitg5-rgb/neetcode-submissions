fd9549124a7100520e4a3a389fa8c3767a107d23
varshitg5-rgb <varshitg5@gmail.com>
2026-07-05 00:50:08 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto x : stones) pq.push(x);
+        while(pq.size()>1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x==y) continue;
+            pq.push(abs(y-x));
+        }
+        
+        return (pq.empty())?0:pq.top();
+    }
+};
