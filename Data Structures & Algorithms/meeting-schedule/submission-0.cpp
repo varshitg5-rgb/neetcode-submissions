8c505f9b6e07da04ff9cccdc5cfda87d896de497
varshitg5-rgb <varshitg5@gmail.com>
2026-07-06 22:21:41 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    static bool comp(const Interval& i1, const Interval& i2){
+        return i1.start<i2.start;
+    }
+
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(),intervals.end(),comp);
+
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i].start<intervals[i-1].end) return false;
+        }
+        return true;
+    }
+};
